d229c1ef01c5b7b6a7c2677438574b633dec904d
Jingxuan <87669275+Taosit@users.noreply.github.com>
2022-10-31 21:41:37 -0700

    airplane rotation and form validation

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 const sendButton = document.querySelector(".send-button");
 const contactCard = document.querySelector(".contact-card");
-const sections = document.querySelectorAll("section")
+const sections = document.querySelectorAll("section");
+
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const messageInput = document.querySelector("#message");
+
+const nameErrorMessage = document.querySelector(".name-error-message");
+const emailErrorMessage = document.querySelector(".email-error-message");
+const messageErrorMessage = document.querySelector(".message-error-message");
 
 const navItemContainer = document.querySelector(".nav-items");
 const navItems = navItemContainer.querySelectorAll("span");
@@ -8,33 +16,147 @@ const navItems = navItemContainer.querySelectorAll("span");
 const navCollapsItemContainer = document.querySelector(".nav-collapse");
 const navCollopasItems = navCollapsItemContainer.querySelectorAll("div");
 
-sendButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    contactCard.classList.add("sent");
+const airplaneContainer = document.querySelector(".airplane-container");
+
+let rotationDegrees = 0;
+let airplaneTimer;
+
+const inputChecker = {
+  name: {isValid: false, message: ""},
+  email: {isValid: false, message: ""},
+  message: {isValid: false, message: ""},
+}
+
+const errorMessageElements = {
+  "name": nameErrorMessage,
+  "email": emailErrorMessage,
+  "message": messageErrorMessage
+}
+
+const updateErrorMessage = (field) => {
+  const errorMessageElement = errorMessageElements[field];
+  if (inputChecker[field].message !== "") {
+    errorMessageElement.textContent = inputChecker[field].message;
+    errorMessageElement.parentElement.classList.remove("opacity-zero");
+  } else {
+    errorMessageElement.innerHTML = "&nbsp;";
+    errorMessageElement.parentElement.classList.add("opacity-zero");
+  }
+}
+
+const updateFormStatus = (field) => {
+  errorMessageElements[field].innerHTML = "&nbsp;";
+  errorMessageElements[field].parentElement.classList.add("opacity-zero");
+  const canSubmit = Object.values(inputChecker).every(input => input.isValid);
+  sendButton.disabled = !canSubmit;
+}
+
+nameInput.addEventListener("input", (e) => {
+  const inputValue = e.target.value;
+
+  if (inputValue.length === 0) {
+    inputChecker.name.isValid = false;
+    inputChecker.name.message = "Name must be filled.";
+  } else if (inputValue.length < 3) {
+    inputChecker.name.isValid = false;
+    inputChecker.name.message = "Name is too short.";
+  } else {
+    inputChecker.name.isValid = true;
+    inputChecker.name.message = "";
+  }
+
+  updateFormStatus("name");
 })
 
-window.onscroll = (e) => {
-    let current = "home";
-  
-    sections.forEach((section) => {
-      const sectionTop = section.offsetTop;
-      if (scrollY >= sectionTop - 48 ) {
-        console.log({screenY:e.scrollY, sectionTop})
-        current = section.getAttribute("id"); }
-    });
-    
-    console.log({current})
-    navItems.forEach((span) => {
-    span.classList.remove("active");
-      if ([...span.classList].some(className => className.includes(current))) {
-        span.classList.add("active");
-      }
-    });
+emailInput.addEventListener("input", (e) => {
+  const inputValue = e.target.value;
 
-    navCollopasItems.forEach((span) => {
-      span.classList.remove("active-item-collapse");
-        if ([...span.classList].some(className => className.includes(current))) {
-          span.classList.add("active-item-collapse");
-        }
-      });
-  };
+  const regex = /^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/;
+  if (inputValue.length === 0) {
+    inputChecker.email.isValid = false;
+    inputChecker.email.message = "Email must be filled.";
+  } else if (!regex.test(inputValue)) {
+    inputChecker.email.isValid = false;
+    inputChecker.email.message = "Invalid email.";
+  } else {
+    inputChecker.email.isValid = true;
+    inputChecker.email.message = "";
+  }
+
+  updateFormStatus("email");
+})
+
+messageInput.addEventListener("input", (e) => {
+  const inputValue = e.target.value;
+
+  if (inputValue.length === 0) {
+    inputChecker.message.isValid = false;
+    inputChecker.message.message = "Message must be filled.";
+  } else if (inputValue.length <= 10) {
+    inputChecker.message.isValid = false;
+    inputChecker.message.message = "Message is too short.";
+  } else {
+    inputChecker.message.isValid = true;
+    inputChecker.message.message = "";
+  }
+
+  updateFormStatus("message");
+})
+
+nameInput.addEventListener("blur", () => updateErrorMessage("name"))
+emailInput.addEventListener("blur", () => updateErrorMessage("email"));
+messageInput.addEventListener("blur", () => updateErrorMessage("message"));
+
+const getCurrentSection = () => {
+  let current = "home";
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (scrollY >= sectionTop - 48 ) {
+      current = section.getAttribute("id"); 
+    }
+  });
+  return current;
+}
+
+const updateNavItems = (currentSection, navItems, activeClassName) => {
+  navItems.forEach((span) => {
+    span.classList.remove(activeClassName);
+    if ([...span.classList].some(className => className.includes(currentSection))) {
+      span.classList.add(activeClassName);
+    }
+  });
+}
+
+const balanceAirplane = () => {
+  if (airplaneTimer) return;
+  airplaneTimer = setInterval(() => {
+    if (rotationDegrees > 1) {
+      rotationDegrees -= 0.08;
+    } else if (rotationDegrees < 0) {
+      rotationDegrees += 0.08;
+    } else {
+      clearInterval(airplaneTimer);
+      airplaneTimer = null;
+    }
+    airplaneContainer.style.transform = `rotate(${rotationDegrees}deg)`
+  }, 1000 / 60)
+}
+
+const rotateAirplane = () => {
+  balanceAirplane()
+  const scrollDirection = scrollY - previousScrollY;
+  rotationDegrees += scrollDirection * -0.02;
+  airplaneContainer.style.transform = `rotate(${rotationDegrees}deg)`
+  previousScrollY = scrollY
+}
+
+let previousScrollY = 0;
+
+window.onscroll = () => {
+  const current = getCurrentSection();
+  
+  updateNavItems(current, navItems, "active");
+  updateNavItems(current, navCollopasItems, "active-item-collapse");
+
+  if (current === "projects") rotateAirplane();
+};
